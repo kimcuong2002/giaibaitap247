635a5389ccfef8ad688f6244b4ac5b8e6ee8ea12
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-12-14 18:15:25 +0700

    feat: create useCreateSubjectMutation, useUpdateSubjectMutation and useDeleteSubjectMutation

--- src/feature/hooks/use-subject-store.ts
+++ src/feature/hooks/use-subject-store.ts
@@ -1,5 +1,11 @@
 import { createQueryKeys } from '@lukemorales/query-key-factory';
-import { QueryOptions, UseQueryResult, useQuery } from '@tanstack/react-query';
+import {
+  QueryOptions,
+  UseQueryResult,
+  useQuery,
+  useMutation,
+} from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 
 import subjectApi from '../../api/subject.api';
 import { ResponseDatas, SubjectType } from '../../interface/subject-type';
@@ -30,5 +36,41 @@ export const useSubjectDetail = (
 ): UseQueryResult<SubjectType, unknown> => {
   return useQuery({
     ...subjects.getDetail(_id),
+  });
+};
+
+export const useCreateSubjectMutation = () => {
+  return useMutation({
+    mutationFn: subjectApi.create,
+    onSuccess: () => {
+      void toast.success('Create new subject successfully!');
+    },
+    onError: () => {
+      void toast.error('Create new subject failed!');
+    },
+  });
+};
+
+export const useUpdateSubjectMutation = () => {
+  return useMutation({
+    mutationFn: subjectApi.update,
+    onSuccess: () => {
+      void toast.success('Update subject successfully!');
+    },
+    onError: () => {
+      void toast.error('Update subject failed!');
+    },
+  });
+};
+
+export const useDeleteSubjectMutation = () => {
+  return useMutation({
+    mutationFn: subjectApi.delete,
+    onSuccess: () => {
+      void toast.success('Delete subject successfully!');
+    },
+    onError: () => {
+      void toast.error('Delete subject failed!');
+    },
   });
 };
