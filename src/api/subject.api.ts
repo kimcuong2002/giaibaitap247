07ff0f94c438ae539d5b795928ff6c0483182a12
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-12-14 18:14:08 +0700

    feat: config endpoints api

--- src/api/subject.api.ts
+++ src/api/subject.api.ts
@@ -1,13 +1,21 @@
 import axiosInstance from './config';
 import { ResponseDatas, SubjectType } from '../interface/subject-type';
-import { getSubjectDetail } from '../utils';
 
-const subjectURL = String(import.meta.env.VITE_API_SUBJECT);
-
+const subjectURL = '/v1/subjects';
 const subjectApi = {
-  getSubjects: (): Promise<ResponseDatas> => axiosInstance.get(subjectURL),
+  getSubjects: (): Promise<ResponseDatas> => axiosInstance.get(`${subjectURL}`),
+
   getDetail: (_id: string): Promise<SubjectType> =>
-    axiosInstance.get(getSubjectDetail(_id)),
+    axiosInstance.get(`${subjectURL}/${_id}`),
+
+  create: (body: SubjectType): Promise<SubjectType> =>
+    axiosInstance.post(`${subjectURL}`, body),
+
+  update: (body: SubjectType): Promise<SubjectType> =>
+    axiosInstance.patch(`${subjectURL}`, body),
+
+  delete: (_id: string): Promise<{ message: string }> =>
+    axiosInstance.delete(`${subjectURL}/${_id}`),
 };
 
 export default subjectApi;
