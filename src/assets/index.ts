e465ecd7c86e5ba1cfe054c9daa110be742f03de
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-12-25 13:28:54 +0700

    feat: import and export images in index.ts

---
+++ src/assets/index.ts
@@ -0,0 +1,55 @@
+import amnhac from './images/amnhac.png';
+import chat from './images/chat.png';
+import congnghe from './images/congnghe.png';
+import diali from './images/diali.png';
+import edit_profile from './images/edit-profile.png';
+import english from './images/english.png';
+import flag from './images/flagflag.png';
+import gdcd from './images/gdcd.png';
+import handle from './images/handle.png';
+import hoahoc from './images/hoahoc.png';
+import khtn from './images/khtn.png';
+import lichsu from './images/lichsu.png';
+import log_out from './images/log-out.png';
+import logo from './images/logo.png';
+import math from './images/math.png';
+import medal1 from './images/medal1.png';
+import menu from './images/menu_icon.svg';
+import nguvan from './images/nguvan.png';
+import profile from './images/profile.png';
+import question1 from './images/question1.png';
+import question2 from './images/question2.png';
+import rank2 from './images/rank2.svg';
+import sinhhoc from './images/sinhhoc.png';
+import su_dia from './images/su&dia.png';
+import tinhoc from './images/tinhoc.png';
+import vatli from './images/vatli.png';
+
+export {
+  flag,
+  rank2,
+  profile,
+  chat,
+  edit_profile,
+  log_out,
+  medal1,
+  handle,
+  menu,
+  amnhac,
+  congnghe,
+  diali,
+  english,
+  gdcd,
+  hoahoc,
+  khtn,
+  lichsu,
+  math,
+  sinhhoc,
+  su_dia,
+  vatli,
+  nguvan,
+  tinhoc,
+  logo,
+  question1,
+  question2,
+};
