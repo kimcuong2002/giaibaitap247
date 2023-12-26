9ebb138e4cf71c0ea84c35657308674fadea5f36
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-12-26 11:13:48 +0700

    fix: config query provider

--- src/provider/query-provider.tsx
+++ src/provider/query-provider.tsx
@@ -1,16 +1,12 @@
 import { FC, ReactNode, useState } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
-
-import { useToast } from '../hooks';
 
 type Props = {
   children: ReactNode;
 };
 
 const QueryProvider: FC<Props> = ({ children }) => {
-  const { toast } = useToast();
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -18,17 +14,7 @@ const QueryProvider: FC<Props> = ({ children }) => {
           queries: {
             refetchOnWindowFocus: false,
             retry: false,
-            onError: (error: unknown) => {
-              void toast.error(
-                `Something went wrong: ${
-                  (
-                    error as AxiosError<{
-                      message: string;
-                    }>
-                  )?.response?.data.message || 'unknown'
-                }`,
-              );
-            },
+            throwOnError: true,
           },
         },
       }),
